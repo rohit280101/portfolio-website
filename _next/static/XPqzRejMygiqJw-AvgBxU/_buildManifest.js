9db30f571396d4943ea9736c26ddf852098e62db
rohit280101 <rohit280101@users.noreply.github.com>
2025-12-09 22:59:19 +0000

    deploy: 711cfb6619225b8a24d9e22115427c6a2bd945f3

---
+++ _next/static/XPqzRejMygiqJw-AvgBxU/_buildManifest.js
@@ -0,0 +1,15 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "static/chunks/45de3f9f533d1145.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error",
+    "/api/contact"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
