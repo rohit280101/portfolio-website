5a3b7f0dcdd35c6d462a7ea281fd075ba7757421
Rohit Gupta <guptarohitr68n@gmail.com>
2025-12-10 00:14:07 +0100

    Set basePath for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+const repoName = 'portfolio-website';
+
 module.exports = {
   reactStrictMode: true,
   output: 'export',
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
